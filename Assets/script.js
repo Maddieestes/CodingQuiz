4050af411bab529af5171605bd2d57f616074998
Madeline Estes <maddiegirl.360@gmail.com>
2023-05-09 14:35:23 -0700

    commit

---
+++ Assets/script.js
@@ -0,0 +1,22 @@
+// global variables for questions, wins, losses, timer, etc
+// what other kind of variables might I need?
+​
+// when user hits start, begin the game
+    // starting the timer
+    // render questions to page
+    // render choices to page
+​
+// if user clicks wrong answer
+    // play annoying sound
+    // move on to next question
+​
+//  if user clicks right answer
+    // give them a point
+    // move on to next question
+​
+// how can I store multiple question objects?
+var questionObj = {
+    question: "May the 4th be with you?",
+    options: ["yes", "no"],
+    correctAnswer: "yes"
+}
